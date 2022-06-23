55645c1eed917c1982a55f032fed4a218d208d1d
Adán de la Rosa Lugo <alu0100909012@ull.edu.es>
2022-06-23 14:02:37 +0100

    refactor adding new test 🔎

--- __test__/hiddenWord.test.ts
+++ __test__/hiddenWord.test.ts
@@ -3,5 +3,17 @@ import { decryptWord } from '../src/hiddenWord';
 describe("decryptWord function should return", () =>{
   test("a when recieves 6", () =>{
     expect(decryptWord(6)).toBe("a");
-  })
+  });
+  test("ab when recieves 61", () =>{
+    expect(decryptWord(61)).toBe("ab");
+  });
+  test("led when recieves 247", () =>{
+    expect(decryptWord(247)).toBe("led");
+  });
+  test("tone when recieves 5084", () =>{
+    expect(decryptWord(5084)).toBe("tone");
+  });
+  test("ERROR when recieves 508.5", () =>{
+    expect(decryptWord(508.5)).toBe("ERROR");
+  });
 });
--- src/hiddenWord.ts
+++ src/hiddenWord.ts
@@ -1,4 +1,37 @@
 
+const dictionary = {
+  "1": "b",
+  "2": "l",
+  "3": "i",
+  "4": "e",
+  "5": "t",
+  "6": "a",
+  "7": "d",
+  "8": "n",
+  "9": "m",
+  "0": "o",
+}
 export const decryptWord = (encryptedWord: number): string => {
-  return "";
+  let decryptedWord: string = "";
+  const encryptedWordString = encryptedWord.toString().split("");
+  const isValidString = checkValidString(encryptedWordString);
+  if(!isValidString)
+    return "ERROR"
+  encryptedWordString.forEach(element => decryptedWord += decodeLetter(element));
+  
+  return decryptedWord;
+};
+
+export const decodeLetter = (letter: string): string => {
+  return dictionary[letter];
+};
+
+export const checkValidString = (encryptedWord: string[]): boolean => {
+  let isValid: boolean = true;
+  encryptedWord.forEach((element) =>{
+    if(!Number.isInteger(parseInt(element)))
+      isValid = false;
+    
+  });
+  return isValid;
 };
